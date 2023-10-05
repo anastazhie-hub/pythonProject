17dfce6d136bdc071353d9a641beae1364ed435f
anastazhie-hub <ganni89@mail.ru>
2023-10-05 12:44:41 +0100

    The second commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def simple_test(a, b):
+    if a == b:
+        print('Тест пройден')
+    else:
+        print('Тест провален')
+
+simple_test(5, '5')
+simple_test(5, 5)
+simple_test(6, 100)
---
+++ simple_count_test.py
@@ -0,0 +1,17 @@
+def test_sum():
+    sum = 4 + 16
+    assert sum == 20
+
+def test_div():
+    num1 = 10
+    num2 = 0
+    assert num2 != 0, 'The divisor is zero='
+
+def test_division():
+    num1 = 10
+    num2 = 2
+    assert num1 // num2 == 5
+
+
+
+
